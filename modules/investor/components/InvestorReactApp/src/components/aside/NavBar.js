113eb17be7977693d86cd7c39f893640ce1e8b06
mihailnesterov <mhause@mail.ru>
2022-03-12 23:57:08 +0700

    ref: NavBar component

--- modules/investor/components/InvestorReactApp/src/components/aside/NavBar.js
+++ modules/investor/components/InvestorReactApp/src/components/aside/NavBar.js
@@ -1,41 +1,14 @@
-import React, {useState} from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { investorRoutes } from '../../routes';
-import {getInitialItemActive, setPageTitle} from '../../utils/navbar';
-
+import React from 'react';
+import NavBarList from './NavBarList';
+import NavBarToggler from './NavBarToggler';
 
 const NavBar = () => {
-    
-    const location = useLocation();
-
-    const [currPathname] = useState(location.pathname);
-    const [isItemActive, setItemActive] = useState(() => 
-        getInitialItemActive(investorRoutes, currPathname)
-    );
-
-    const itemActiveHandler = (item) => {
-        setItemActive(item.id);
-        setPageTitle(item.title);
-    }
-
     return (
         <nav className='navbar'>
-            <ul>
-                {
-                    investorRoutes.map(item => 
-                        item.navbar &&
-                        <li key={item.id} 
-                            className={isItemActive === item.id ? 'active' : null} 
-                            onClick={() => itemActiveHandler(item)}
-                        >
-                            <Link to={item.path}>{item.icon}{item.title}</Link>
-                        </li>
-                    )
-                }
-            </ul>
+            <NavBarList />
+            <NavBarToggler />
         </nav>
     )
 }
-
 
 export default NavBar;
