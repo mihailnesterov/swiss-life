55b9be7013161a106fa88064f9b9a1c16c9970ac
mihailnesterov <mhause@mail.ru>
2022-03-14 16:04:42 +0700

    add: InvestForm component

---
+++ modules/investor/components/InvestorReactApp/src/components/investment/InvestForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvestForm = () => {
+
+    return (
+        <div className='invest-form'>
+            invest-form
+        </div>
+    )
+}
+
+export default InvestForm;
