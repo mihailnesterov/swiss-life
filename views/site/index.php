5d8b14ec533a44681a75f841ef6bbe1be23bc9d6
mihailnesterov <mhause@mail.ru>
2022-03-16 05:13:54 +0700

    update: views index home page

--- views/site/index.php
+++ views/site/index.php
@@ -10,53 +10,64 @@ use yii\widgets\ActiveForm;
 
 <main role="main">
 
-    <header style="text-align: center">
-        <h1><?= Html::encode($this->title) ?></h1>
+    <header class="header">
+        <h1><?= Html::encode(\Yii::$app->name) ?></h1>
     </header>
 
-    <div class="login-container" style="margin: 0 auto; max-width: 480px;">
+    <div class="container">
+
+        <div class="content">
+            <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+            <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
+            <p>Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        </div>
+
+        <div class="login">
+
+                <?php $form = ActiveForm::begin(); ?>
+
+                    <?= $form->field($model, 'email', [
+                        'template' => '{input}{error}',
+                        'inputOptions' => [
+                            'autofocus' => 'autofocus',
+                            'tabindex' => '1',
+                            'placeholder' => 'Email',
+                            'class'=>'input',
+                            //'pattern'=>'\D+([a-zA-Z0-9._@])$'
+                        ]
+                    ])->label(false) ?>
 
 
-            <?php $form = ActiveForm::begin(); ?>
+                    <?= $form->field($model, 'password', [
+                        'template' => '{input}{error}',
+                        'inputOptions' => [
+                            'tabindex' => '2',
+                            'placeholder' => 'Пароль',
+                            'class'=>'input'
+                        ]
+                    ])->passwordInput()->label(false) ?>
 
-                <?= $form->field($model, 'email', [
-                    'template' => '{input}{error}',
-                    'inputOptions' => [
-                        'autofocus' => 'autofocus',
-                        'tabindex' => '1',
-                        'placeholder' => 'Email',
-                        'class'=>'form-control input-lg',
-                        //'pattern'=>'\D+([a-zA-Z0-9._@])$'
-                    ]
-                ])->label(false) ?>
+                    <?= $form->field($model, 'rememberMe', [
+                        'template' => '{input}{error}',
+                        'inputOptions' => [
+                            'tabindex' => '3',
+                            'class'=>'checkbox'
+                        ]
+                    ])->checkbox(['value' => 0, 'checked' => false]) ?>
 
+                    <div class="form-submit-group">
+                        <?= Html::submitButton('Войти', ['class' => 'btn-login']) ?>
+                        <a href="<?=Yii::$app->urlManager->createUrl(['password-restore'])?>" class='btn-link'>Забыли пароль?</a>
+                    </div>
+                <?php ActiveForm::end(); ?>
 
-                <?= $form->field($model, 'password', [
-                    'template' => '{input}{error}',
-                    'inputOptions' => [
-                        'tabindex' => '2',
-                        'placeholder' => 'Пароль',
-                        'class'=>'form-control input-lg'
-                    ]
-                ])->passwordInput()->label(false) ?>
+        </div>
 
-                <?= $form->field($model, 'rememberMe', [
-                    'template' => '{input}{error}',
-                    'inputOptions' => [
-                        'tabindex' => '3',
-                        'class'=>'form-control input-lg'
-                    ]
-                ])->checkbox(['value' => 0, 'checked' => false]) ?>
+    </div>
 
-                <div class="form-group  text-center">
-                    <?= Html::submitButton('Войти', ['class' => 'btn-login']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-
-            <div class="login-links">
-                <a href="<?=Yii::$app->urlManager->createUrl(['password-restore'])?>">Забыли пароль?</a>
-            </div>
-
-    </div><!-- end login-container -->
+    <footer>
+        <p class="copyright"><?= Html::encode(\Yii::$app->name) ?> &copy; <?= date('Y') ?></p>
+    </footer>
    
 </main>
