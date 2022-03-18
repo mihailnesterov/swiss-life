75b148cdd90ff925ea4b5e39a2e2ec255cd63ce0
mihailnesterov <mhause@mail.ru>
2022-03-18 16:10:00 +0700

    update: UploadImageFile input type file accept image

--- modules/investor/components/InvestorReactApp/src/components/common/form/UploadImageFile.js
+++ modules/investor/components/InvestorReactApp/src/components/common/form/UploadImageFile.js
@@ -24,6 +24,7 @@ const UploadImageFile = (props) => {
             <label>
                 <input 
                     type="file"
+                    accept="image/*"
                     onChange={handleFileUpload} 
                 />
                 <img 
