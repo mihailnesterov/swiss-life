ed70fff373fe4cae1b7bd8b47b154f45293bc629
mihailnesterov <mhause@mail.ru>
2022-03-20 09:14:25 +0700

    add: StatusController

---
+++ api/modules/v1/controllers/StatusController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\common\controllers\BaseApiController;
+
+class StatusController extends BaseApiController
+{
+    public $modelClass = 'app\models\Status';
+}
