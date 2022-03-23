d7c9ddb296b0a6ee979b596d4909760f88f6b1f1
mihailnesterov <mhause@mail.ru>
2022-03-23 15:41:27 +0700

    add: MembersListHead component

---
+++ modules/investor/components/InvestorReactApp/src/components/members/MembersListHead.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MembersListHead = () => {
+
+    return (
+        <div>
+            <h4>№</h4>
+            <h4>ФИО</h4>
+            <h4>Email</h4>
+            <h4>Активен</h4>
+            <h4>Верифицирован</h4>
+        </div>
+    )
+}
+
+export default MembersListHead;
