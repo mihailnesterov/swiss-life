219743ded95660ac1a6e8d255bc0c9e640e73fa4
mihailnesterov <mhause@mail.ru>
2022-03-23 16:35:12 +0700

    update: routes add contracts route

--- modules/investor/components/InvestorReactApp/src/routes/index.js
+++ modules/investor/components/InvestorReactApp/src/routes/index.js
@@ -16,6 +16,7 @@ import Withdrawals from '../pages/Withdrawals';
 import Transfer from '../pages/Transfer';
 import AddAccountPage from '../pages/AddAccountPage';
 import Members from '../pages/Members';
+import Contracts from '../pages/Contracts';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
@@ -36,6 +37,7 @@ export const RouteNames = Enum({
     TRANSFER: `${BASE_URL}/transfer`,
     ADD_ACCOUNT: `${BASE_URL}/add-account`,
     MEMBERS: `${BASE_URL}/members`,
+    CONTRACTS: `${BASE_URL}/contracts`,
     LOGOUT: `${BASE_URL}/logout`,
 });
 
@@ -49,10 +51,11 @@ export const investorRoutes = [
     {id: 7, path: RouteNames.MESSAGES, element: Messages, navbar: true, icon: <FontAwesomeIcon icon={solid('envelope')} />, title: "Сообщения"},
     {id: 8, path: RouteNames.MEMBERS, element: Members, navbar: true, icon: <FontAwesomeIcon icon={solid('user-group')} />, title: "Подписчики"},
     {id: 9, path: RouteNames.DOCUMENTS, element: Documents, navbar: true, icon: <FontAwesomeIcon icon={solid('folder-open')} />, title: "Документы"},
-    {id: 10, path: RouteNames.PROFILE, element: Profile, navbar: false, icon: <FontAwesomeIcon icon={solid('user-tie')} />, title: "Мой профиль"},
-    {id: 11, path: RouteNames.ERROR404, element: Error404, navbar: false, title: "404 ошибка"},
-    {id: 12, path: RouteNames.NEWS_ITEM_PAGE, element: NewsItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('newspaper')} />, exact: true, title: "Новость"},
-    {id: 13, path: RouteNames.INVESTMENT_ITEM_PAGE, element: InvestmentItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Объект инвестирования"},
-    {id: 14, path: RouteNames.INVEST_PAGE, element: InvestPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Инвестировать в объект"},
-    {id: 15, path: RouteNames.ADD_ACCOUNT, element: AddAccountPage, navbar: false, icon: <FontAwesomeIcon icon={solid('coins')} />, title: "Открыть счет"},
+    {id: 10, path: RouteNames.CONTRACTS, element: Contracts, navbar: true, icon: <FontAwesomeIcon icon={solid('file-contract')} />, title: "Договоры"},
+    {id: 11, path: RouteNames.PROFILE, element: Profile, navbar: false, icon: <FontAwesomeIcon icon={solid('user-tie')} />, title: "Мой профиль"},
+    {id: 12, path: RouteNames.ERROR404, element: Error404, navbar: false, title: "404 ошибка"},
+    {id: 13, path: RouteNames.NEWS_ITEM_PAGE, element: NewsItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('newspaper')} />, exact: true, title: "Новость"},
+    {id: 14, path: RouteNames.INVESTMENT_ITEM_PAGE, element: InvestmentItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Объект инвестирования"},
+    {id: 15, path: RouteNames.INVEST_PAGE, element: InvestPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Инвестировать в объект"},
+    {id: 16, path: RouteNames.ADD_ACCOUNT, element: AddAccountPage, navbar: false, icon: <FontAwesomeIcon icon={solid('coins')} />, title: "Открыть счет"},
 ];
