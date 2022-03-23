c568ccb3846d3189e90c4a47959482ab2cd8ce3f
mihailnesterov <mhause@mail.ru>
2022-03-23 21:41:29 +0700

    add: Contract model

---
+++ models/Contract.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contract".
+ *
+ * @property int $id id договора
+ * @property int $file_id id файла
+ * @property string $description Описание договора
+ * @property string $created Дата создания
+ *
+ * @property File $file
+ */
+class Contract extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contract';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['file_id'], 'required'],
+            [['file_id'], 'integer'],
+            [['created'], 'safe'],
+            [['description'], 'string', 'max' => 255],
+            [['file_id'], 'exist', 'skipOnError' => true, 'targetClass' => File::className(), 'targetAttribute' => ['file_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'id договора',
+            'file_id' => 'id файла',
+            'description' => 'Описание договора',
+            'created' => 'Дата создания',
+        ];
+    }
+
+    /**
+     * Gets query for [[File]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFile()
+    {
+        return $this->hasOne(File::className(), ['id' => 'file_id']);
+    }
+}
