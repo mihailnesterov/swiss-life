f496a6d4b86dbdc2fac2bd1cc814b01cf8e75dbf
mihailnesterov <mhause@mail.ru>
2022-03-23 16:13:26 +0700

    update: User API remove user's status expand

--- modules/investor/components/InvestorReactApp/src/api/user.js
+++ modules/investor/components/InvestorReactApp/src/api/user.js
@@ -2,7 +2,7 @@
 import API, {setHeaders, credentials} from './index';
 
 const URL = `/users`;
-const EXPAND = `expand=userAssets,manager,accounts,userDocuments,userPhotos,messages,userCategories,userStatus`;
+const EXPAND = `expand=userAssets,manager,accounts,userDocuments,userPhotos,messages,userCategories`;
 
 export const getUserAuthorized = async () => 
     await API.get(`${URL}/authorized`, { credentials, setHeaders });
