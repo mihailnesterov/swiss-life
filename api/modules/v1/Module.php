046b9296a628d837b38fa4faa592d0a0541374db
mihailnesterov <mhause@mail.ru>
2022-03-25 01:03:01 +0700

    update: change CORS cridentials header option to false

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -59,7 +59,7 @@ class Module extends \yii\base\Module
                     'cors'  => [
                         'Origin'                           => static::allowedDomains(),
                         'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                        'Access-Control-Allow-Credentials' => true,
+                        'Access-Control-Allow-Credentials' => false,
                         'Access-Control-Max-Age'           => 3600,
 
                     ],
--- modules/investor/controllers/DefaultController.php
+++ modules/investor/controllers/DefaultController.php
@@ -55,7 +55,7 @@ class DefaultController extends Controller
                 'cors'  => [
                     'Origin'                           => static::allowedDomains(),
                     'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Max-Age'           => 3600,
 
                 ],
