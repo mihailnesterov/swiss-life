e3330409de326f599c276e8ea3739e8c71a44e06
mihailnesterov <mhause@mail.ru>
2022-03-26 18:24:34 +0700

    update: ManagerController add users, accounts, messages actions

--- api/modules/v1/controllers/ManagerController.php
+++ api/modules/v1/controllers/ManagerController.php
@@ -9,4 +9,28 @@ class ManagerController extends BaseApiController
 {
     public $modelClass = 'app\models\Manager';
 
+    public function actionUsers($id)
+    {
+        return \app\models\User::find()->where(['manager_id' => $id])->all();
+    }
+
+    public function actionAccounts($id)
+    {
+        return new \yii\data\ActiveDataProvider([
+            'query' => \app\models\Account::find()
+                ->where(['manager_id' => $id])
+                ->joinWith('user', false)
+        ]);
+    }
+
+    public function actionMessages($id)
+    {
+        return new \yii\data\ActiveDataProvider([
+            'query' => \app\models\Message::find()
+                ->orderBy(['created' => SORT_DESC])
+                ->where(['manager_id' => $id]),
+            'pagination' => $this->pagination,
+        ]);
+    }
+
 }
