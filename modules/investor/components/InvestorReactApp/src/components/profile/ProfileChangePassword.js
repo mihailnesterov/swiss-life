792b07d54b0f0ce69144c518f6203c130bd31927
mihailnesterov <mhause@mail.ru>
2022-03-27 03:47:10 +0700

    update: ProfileChangePassword component add input changed local state and on submit handler

--- modules/investor/components/InvestorReactApp/src/components/profile/ProfileChangePassword.js
+++ modules/investor/components/InvestorReactApp/src/components/profile/ProfileChangePassword.js
@@ -7,16 +7,30 @@ const ProfileChangePassword = (props) => {
     const {user} = props;
 
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [newPassword, setNewPassword] = useState(null);
+
+    const onNewPasswordChangeHandler = (e) => {
+        setNewPassword(e.target.value);
+        if( e.target.value === '' )
+            setNewPassword(null);
+    }
+
+    const handleSubmitForm = e => {
+        e.preventDefault();
+        setNewPassword(null);
+    };
 
     return (
         <div className='form-container'>
-            <form>
+            <form onSubmit={handleSubmitForm}>
                 <h3>Изменить пароль</h3>
                 <fieldset>
                     <div className='input-change-password'>
                         <input 
                             type={!isPasswordVisible ? "password" : "text"}
                             placeholder='Новый пароль'
+                            onChange={onNewPasswordChangeHandler}
+                            autoComplete='off'
                         />
                         <button
                             type="button"
@@ -30,9 +44,8 @@ const ProfileChangePassword = (props) => {
                             }
                         </button>
                     </div>
-                    
                 </fieldset>
-                <button type='submit'>Сохранить</button>
+                <button disabled={newPassword ? false : true} type='submit'>Сохранить</button>
             </form>
         </div>
     )
