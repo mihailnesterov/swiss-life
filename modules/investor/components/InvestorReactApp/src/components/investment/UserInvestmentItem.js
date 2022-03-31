6ee45326a03f46e2048f72edf3019badbd80ddd3
mihailnesterov <mhause@mail.ru>
2022-03-31 17:07:46 +0700

    update: UserInvestmentItem component change props fields

--- modules/investor/components/InvestorReactApp/src/components/investment/UserInvestmentItem.js
+++ modules/investor/components/InvestorReactApp/src/components/investment/UserInvestmentItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {getDateToString} from '../../utils/dates';
 
-
 const UserInvestmentItem = (props) => {
 
     const {item} = props;
 
     return (
         <div>
-            <p>Объект: <b>{item.asset.name}</b></p>
+            <p>Объект: <b>{item.name}</b></p>
             <p>Дата: <b>{getDateToString(item.created)}</b></p>
-            <p>Сумма: <b>{item.sum} {item.currency.sign}</b></p>
+            <p>Сумма: <b>{item.sum} {item.sign}</b></p>
         </div>
     )
 }
