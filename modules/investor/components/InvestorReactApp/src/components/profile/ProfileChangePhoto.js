2e9d67db18a35ed3c10c499c144e4e8bcd64930d
mihailnesterov <mhause@mail.ru>
2022-04-03 09:08:27 +0700

    update: ProfileChangePhoto component add photo uploading

--- modules/investor/components/InvestorReactApp/src/components/profile/ProfileChangePhoto.js
+++ modules/investor/components/InvestorReactApp/src/components/profile/ProfileChangePhoto.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import UploadImageFile from '../common/form/UploadImageFile';
+import {uploadFile} from '../../api/file';
+import {setUserPhoto} from '../../api/user';
 
 const ProfileChangePhoto = (props) => {
 
@@ -10,34 +12,34 @@ const ProfileChangePhoto = (props) => {
     useEffect(() => {
         if( user.userPhotos && user.userPhotos.length > 0 ) {
             setSelectedFile({
-                src: user.userPhotos[0].url,
-                alt: user.userPhotos[0].name
+                data: user.userPhotos[0].url,
+                name: user.userPhotos[0].name,
+                ext: user.userPhotos[0].extention
             });
         }
     }, [user]);
-
+    
     const [isImageChanged, setImageChanged] = useState(false);
 
     const handleSubmitForm = e => {
         e.preventDefault();
 
         if(selectedFile) {
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-            formData.append('fileName', selectedFile.src);
-            const config = {
-                headers: {
-                    'content-type': 'multipart/form-data',
-                },
-            };
 
-            setImageChanged(false);
-            
-            /*axios.post("api/uploadfile", formData, config).then((response) => {
-                console.log(response.data);
-            });*/
+            const {data, name, ext} = selectedFile;
+            uploadFile({data, name, ext})
+                .then(file => {
+                    console.log('uploadFile', file.data);
+                    setUserPhoto({
+                        user_id: user.id,
+                        file_id: file.data.id,
+                    })
+                        .then(userPhoto => console.log('userPhoto',userPhoto))
+                        .catch(err => console.log(err))
+                        .finally(() => setImageChanged(false));
+                })
+                .catch(err => console.log(err))
         }
-        
     };
 
     return (
