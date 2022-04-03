9a8070d8500b74c3da19ca7003849d03702f2edc
mihailnesterov <mhause@mail.ru>
2022-04-03 08:45:08 +0700

    update: FileController add methods for files uploading and directory creating

--- api/modules/v1/controllers/FileController.php
+++ api/modules/v1/controllers/FileController.php
@@ -16,6 +16,40 @@ class FileController extends BaseApiController
     ];
 
     public function actionCreate_folder()
+    {        
+        return $this->getUploadsPath();
+    }
+
+    public function actionUpload() {
+        if( Yii::$app->request->getBodyParams() ) {
+            $data = Yii::$app->request->getBodyParam('data');
+            $name = Yii::$app->request->getBodyParam('name');
+            $ext = Yii::$app->request->getBodyParam('ext');
+
+            $fileName = $this->getUploadsPath() . "/$name.$ext";
+
+            if( file_put_contents($fileName, file_get_contents($data)) ) {
+                $model = new \app\models\File();
+                $model->name = $name;
+                $model->extention = $ext;
+                $model->save();
+                
+                return $model;
+            }
+
+            return false;
+        }
+    }
+
+    private function getUploadsPath()
+    {
+        $uploadsPath = Yii::$app->basePath . "/web/uploads/" . $this->getCurrentDateAsString();
+        $this->createUploadsFolder($uploadsPath);
+        
+        return $uploadsPath;
+    }
+
+    private function getCurrentDateAsString()
     {
         date_default_timezone_set('Europe/Moscow');
         $date = new \DateTime();
@@ -23,12 +57,13 @@ class FileController extends BaseApiController
         $month = $date->format('m');
         $year = $date->format('Y');
         
-        $uploadsPath = Yii::$app->basePath . "/web/uploads/$year/$month/$day";
-        
+        return "$year/$month/$day";
+    }
+
+    private function createUploadsFolder($uploadsPath)
+    {
         if (!file_exists($uploadsPath)) {
             mkdir($uploadsPath, 0777, true);
         }
-        
-        return $uploadsPath;
     }
 }
