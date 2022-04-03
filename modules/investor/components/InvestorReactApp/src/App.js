677884db6cd49ca95a856cedb49136cbaa09b6ec
mihailnesterov <mhause@mail.ru>
2022-04-03 18:31:14 +0700

    add: ToastContainer into App component

--- modules/investor/components/InvestorReactApp/src/App.js
+++ modules/investor/components/InvestorReactApp/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
@@ -9,9 +11,12 @@ const App = () => {
            <Header />
            <Main />
            <Footer />
+            <ToastContainer 
+                autoClose={3000} 
+                transition={Slide}
+            />
         </div>
     )
 }
-
 
 export default App;
