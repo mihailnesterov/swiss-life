08e82e2e0dcaa6e56c93fdbbe99b480ba2d82f91
mihailnesterov <mhause@mail.ru>
2022-04-09 12:47:04 +0700

    add: UserDocumentController

---
+++ api/modules/v1/controllers/UserDocumentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\common\controllers\BaseApiController;
+
+class UserDocumentController extends BaseApiController
+{
+    public $modelClass = 'app\models\UserDocument';
+}
