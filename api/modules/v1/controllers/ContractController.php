70dae5c696ee00b8f5f3f3efe67425aecdfef240
mihailnesterov <mhause@mail.ru>
2022-04-09 09:21:59 +0700

    update: ContractController add update_file endpoint

--- api/modules/v1/controllers/ContractController.php
+++ api/modules/v1/controllers/ContractController.php
@@ -13,4 +13,31 @@ class ContractController extends BaseApiController
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'contracts'
     ];
+
+    public function actionUpdate_file()
+    {
+        if( Yii::$app->request->getBodyParams() ) {
+            
+            $id = Yii::$app->request->getBodyParam('id');
+            $file_id = Yii::$app->request->getBodyParam('file_id');
+
+            if(!empty($id) && !empty($file_id)) {
+                
+                $contract = \app\models\Contract::find()->where(['id' => $id])->one();
+                
+                if(!empty($contract)) {
+                    $contract->file_id = $file_id;
+                    $contract->save();
+                } else {
+                    $model = new \app\models\Contract();
+                    $model->file_id = $file_id;
+                    $model->save();
+                }
+
+                return $contract;
+            }
+
+            return false;
+        }
+    }
 }
