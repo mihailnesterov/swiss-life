dd25f55b47aad1ea368048063d23473168f32fe7
mihailnesterov <mhause@mail.ru>
2022-04-11 05:35:20 +0700

    fix: company API remove manager expand

--- modules/manager/components/ManagerReactApp/src/api/company.js
+++ modules/manager/components/ManagerReactApp/src/api/company.js
@@ -2,18 +2,10 @@
 import API, {setHeaders, credentials} from './index';
 
 const URL = `/companies`;
-const EXPAND = `expand=assets,managers,partners`;
+const EXPAND = `expand=assets,partners`;
 
-/**
- * Get active company
- * @returns 
- */
 export const getCompany = async () => 
     await API.get(URL, { credentials, setHeaders });
 
-/**
- * Get active company expanded
- * @returns 
- */
 export const getCompanyExpanded = async () => 
     await API.get(`${URL}?${EXPAND}`, { credentials, setHeaders });
