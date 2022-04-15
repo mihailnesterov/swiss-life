c49047bbcaa30690e38b3344611f262e05158aa8
mihailnesterov <mhause@mail.ru>
2022-04-15 10:44:08 +0700

    update: ProfilePage index componrnt add UserForm component import

--- modules/investor/components/InvestorReactApp/src/components/profile/index.js
+++ modules/investor/components/InvestorReactApp/src/components/profile/index.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import ProfileChangePhoto from './ProfileChangePhoto';
 import ProfileChangePassword from './ProfileChangePassword';
 import Spinner from '../common/loader/Spinner';
+import UserForm from '../user/UserForm';
 
 const ProfilePage = () => {
 
@@ -14,6 +15,7 @@ const ProfilePage = () => {
                 loading ?
                 <Spinner size={2} /> :
                 <>
+                    <UserForm user={user} />
                     <ProfileChangePhoto user={user} />
                     <ProfileChangePassword user={user} />
                 </>
