e1a04804e7f3a8973b1db600a577e7b578d7a557
mihailnesterov <mhause@mail.ru>
2022-04-19 00:04:08 +0700

    update: views and css on homepage

--- views/site/index.php
+++ views/site/index.php
@@ -9,62 +9,50 @@ use yii\widgets\ActiveForm;
 ?>
 
 <main role="main">
+        
+    <div class="login">
+        <header class="header">
+            <h1><?= Html::encode(\Yii::$app->name) ?></h1>
+        </header>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <header class="header">
-        <h1><?= Html::encode(\Yii::$app->name) ?></h1>
-    </header>
-
-    <div class="container">
-
-        <div class="content">
-            <img src="images/bg.jpg" alt="background" />
-        </div>
-
-        <div class="login">
-
-                <?php $form = ActiveForm::begin(); ?>
-
-                    <?= $form->field($model, 'email', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'autofocus' => 'autofocus',
-                            'tabindex' => '1',
-                            'placeholder' => 'Email',
-                            'class'=>'input',
-                            //'pattern'=>'\D+([a-zA-Z0-9._@])$'
-                        ]
-                    ])->label(false) ?>
+            <?= $form->field($model, 'email', [
+                'template' => '{input}{error}',
+                'inputOptions' => [
+                    'autofocus' => 'autofocus',
+                    'tabindex' => '1',
+                    'placeholder' => 'Email',
+                    'class'=>'input',
+                    //'pattern'=>'\D+([a-zA-Z0-9._@])$'
+                ]
+            ])->label(false) ?>
 
 
-                    <?= $form->field($model, 'password', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'tabindex' => '2',
-                            'placeholder' => 'Пароль',
-                            'class'=>'input'
-                        ]
-                    ])->passwordInput()->label(false) ?>
+            <?= $form->field($model, 'password', [
+                'template' => '{input}{error}',
+                'inputOptions' => [
+                    'tabindex' => '2',
+                    'placeholder' => 'Пароль',
+                    'class'=>'input'
+                ]
+            ])->passwordInput()->label(false) ?>
 
-                    <?= $form->field($model, 'rememberMe', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'tabindex' => '3',
-                            'class'=>'checkbox'
-                        ]
-                    ])->checkbox(['value' => 0, 'checked' => false]) ?>
+            <?= $form->field($model, 'rememberMe', [
+                'template' => '{input}{error}',
+                'inputOptions' => [
+                    'tabindex' => '3',
+                    'class'=>'checkbox'
+                ]
+            ])->checkbox(['value' => 0, 'checked' => false]) ?>
 
-                    <div class="form-submit-group">
-                        <?= Html::submitButton('Войти', ['class' => 'btn-login']) ?>
-                        <a href="<?=Yii::$app->urlManager->createUrl(['password-restore'])?>" class='btn-link'>Забыли пароль?</a>
-                    </div>
-                <?php ActiveForm::end(); ?>
-
-        </div>
-
+            <div class="form-submit-group">
+                <?= Html::submitButton('Войти', ['class' => 'btn-login']) ?>
+                <a href="<?=Yii::$app->urlManager->createUrl(['password-restore'])?>" class='btn-link'>Забыли пароль?</a>
+            </div>
+        <?php ActiveForm::end(); ?>
+        <footer>
+            <p class="copyright"><?= Html::encode(\Yii::$app->name) ?> &copy; <?= date('Y') ?></p>
+        </footer>
     </div>
-
-    <footer>
-        <p class="copyright"><?= Html::encode(\Yii::$app->name) ?> &copy; <?= date('Y') ?></p>
-    </footer>
    
 </main>
