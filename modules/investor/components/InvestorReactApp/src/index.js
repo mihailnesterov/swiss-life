3b270f34d58d53169cd297f531c87c8602de013e
mihailnesterov <mhause@mail.ru>
2022-05-16 06:30:40 +0700

    update: import lingui provider into root component and activate locales

--- modules/investor/components/InvestorReactApp/src/index.js
+++ modules/investor/components/InvestorReactApp/src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './store/store';
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
+import { en, ru } from 'make-plural/plurals';
+import { messages as enMessages } from './locales/en/messages';
+import { messages as ruMessages } from './locales/ru/messages';
 import App from './App';
 import './theme/dark/style.scss';
 
+i18n.loadLocaleData({
+    en: { plurals: en },
+    ru: { plurals: ru },
+});
+i18n.load({
+    en: enMessages,
+    ru: ruMessages,
+});
+i18n.activate('ru');
+
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
+    <I18nProvider i18n={i18n}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </I18nProvider>,
     document.getElementById('investor-react-app')
 );
