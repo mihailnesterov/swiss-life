2ed03a372585a844bab8bf489556162cd024f4cc
mihailnesterov <mhause@mail.ru>
2022-09-11 02:49:02 +0700

    update: add scroll to anchor and to top link

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,1 +1,20 @@
-new WOW().init();
+window.onload = function() {
+    
+    new WOW().init();
+
+    document.body.onscroll = function() {
+        if ( document.body.getBoundingClientRect().top === 0 ) {
+            document.querySelector('.toTop').style.display = 'none';
+        } else {
+            document.querySelector('.toTop').style.display = 'block';
+        }
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(item => {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+        });
+    });
+    
+}
