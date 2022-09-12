78cdc7447b5258555016ce520790ed05d72963a4
mihailnesterov <mhause@mail.ru>
2022-09-12 07:47:45 +0700

    update: translate login view

--- views/site/login.php
+++ views/site/login.php
@@ -1,63 +1,115 @@
 <?php
 
-use yii\helpers\Html;
+use yii\helpers\{Html, Url};
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 /* @var $form ActiveForm */
+
+$this->registerJs("
+    jQuery('#show-password').change(function(){
+        jQuery('#userlogin-password').attr('type', this.checked ? 'text' : 'password');
+    })"
+);
+
+if( null !== Yii::$app->request->get('lang') ) {
+    Yii::$app->language = Yii::$app->request->get('lang');
+}
+
+$this->title = Yii::t('app', 'Войдите в свой аккаунт или создайте новый');
+
 ?>
-<main role="main">
-    <header class="header">
+<main class="main">
+    <?= Yii::$app->session->getFlash('login') ?>
+    <div class="login">
         <h1><?= Html::encode($this->title) ?></h1>
-    </header>
+        <div class="login-list">
+            <?php $form = ActiveForm::begin([
+                'id' => 'signin',
+                'validateOnType' => true,
+                'fieldConfig' => [
+                    'template' => '<div class="input-block">{label}{input}{error}</div>',
+                ],
+                'options' => [
+                    'class' => 'login-form',
+                ],
+            ]); ?>
 
-    <div class="container">
-
-        <div class="login">
-
-                <?php $form = ActiveForm::begin(); ?>
-
-                    <?= $form->field($model, 'email', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'autofocus' => 'autofocus',
-                            'tabindex' => '1',
-                            'placeholder' => 'Email',
-                            'class'=>'input',
-                            //'pattern'=>'\D+([a-zA-Z0-9._@])$'
-                        ]
-                    ])->label(false) ?>
+                <?= $form->field($model, 'email', [
+                    'inputOptions' => [
+                        'autofocus' => 'autofocus',
+                        'tabindex' => '1',
+                        'placeholder' => Yii::t('app', 'Ваш email'),
+                        'class'=>'input',
+                        //'pattern'=>'\D+([a-zA-Z0-9._@])$'
+                    ]
+                ])->label('Email') ?>
 
 
-                    <?= $form->field($model, 'password', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'tabindex' => '2',
-                            'placeholder' => 'Пароль',
-                            'class'=>'input'
-                        ]
-                    ])->passwordInput()->label(false) ?>
+                <?= $form->field($model, 'password', [
+                    'inputOptions' => [
+                        'tabindex' => '2',
+                        'placeholder' => Yii::t('app', 'Ваш пароль'),
+                        'class'=>'input'
+                    ]
+                ])->passwordInput()->label(Yii::t('app', 'Пароль')) ?>
 
-                    <?= $form->field($model, 'rememberMe', [
-                        'template' => '{input}{error}',
-                        'inputOptions' => [
-                            'tabindex' => '3',
-                            'class'=>'checkbox'
-                        ]
-                    ])->checkbox(['value' => 0, 'checked' => false]) ?>
+                <?= $form->field($model, 'rememberMe', [
+                    'inputOptions' => [
+                        'tabindex' => '3',
+                        'class'=>'checkbox hidden'
+                    ]
+                ])->checkbox(['value' => 0, 'checked' => true]) ?>
 
-                    <div class="form-submit-group">
-                        <?= Html::submitButton('Войти', ['class' => 'btn-login']) ?>
-                        <a href="<?=Yii::$app->urlManager->createUrl(['password-restore'])?>" class='btn-link'>Забыли пароль?</a>
-                    </div>
-                <?php ActiveForm::end(); ?>
+                <div class="form-submit-group">
+                    <?= Html::submitButton(Yii::t('app', 'Войти в аккаунт'), ['class' => 'btn btn-secondary']) ?>
+                    <a href="<?=Yii::$app->urlManager->createUrl(['password-restore', 'lang' => Yii::$app->language])?>" class='btn-link'><?= Yii::t('app', 'Забыли пароль?') ?></a>
+                </div>
+            <?php ActiveForm::end(); ?>
+        
+            <?php $form = ActiveForm::begin([
+                'id' => 'signup',
+                'validateOnType' => true,
+                'fieldConfig' => [
+                    'template' => '<div class="input-block">{label}{input}{error}</div>',
+                ],
+                'options' => [
+                    'class' => 'login-form',
+                ],
+            ]); ?>
 
+                <?= $form->field($model, 'email', [
+                    'inputOptions' => [
+                        'autofocus' => 'autofocus',
+                        'tabindex' => '1',
+                        'placeholder' => Yii::t('app', 'Ваш email'),
+                        'class'=>'input',
+                        //'pattern'=>'\D+([a-zA-Z0-9._@])$'
+                    ]
+                ])->label('Email') ?>
+
+
+                <?= $form->field($model, 'password', [
+                    'inputOptions' => [
+                        'tabindex' => '2',
+                        'placeholder' => Yii::t('app', 'Ваш пароль'),
+                        'class'=>'input'
+                    ]
+                ])->passwordInput()->label(Yii::t('app', 'Придумайте пароль')) ?>
+
+                <?= $form->field($model, 'rememberMe', [
+                    'inputOptions' => [
+                        'tabindex' => '3',
+                        'class'=>'checkbox hidden'
+                    ]
+                ])->checkbox(['value' => 0, 'checked' => true]) ?>
+
+                <div class="form-submit-group">
+                    <?= Html::submitButton(Yii::t('app', 'Создать аккаунт'), ['class' => 'btn btn-primary']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
         </div>
-
     </div>
-    
-    <footer>
-        <p class="copyright"><?= Html::encode(\Yii::$app->name) ?> &copy; <?= date('Y') ?></p>
-    </footer>
+
 </main>
