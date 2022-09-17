4aee13a1165c6b187f8317c5618e2d8b34808706
mihailnesterov <mhause@mail.ru>
2022-09-18 02:11:40 +0700

    update: OrderAccount model add after save new order emails

--- models/OrderAccount.php
+++ models/OrderAccount.php
@@ -63,4 +63,59 @@ class OrderAccount extends \yii\db\ActiveRecord
             'created' => 'Дата создания',
         ];
     }
+
+    /*
+     * After order save
+     */
+    public function afterSave($insert, $changedAttributes) {
+        parent::afterSave($insert, $changedAttributes);
+        
+        if ($insert) {
+            // if new order
+
+            $receivers = Yii::$app->params['company']['emails'];
+
+            foreach($receivers as $receiver) {
+
+                Yii::$app->mailer->getView()->params['name'] = $this->name;
+                Yii::$app->mailer->getView()->params['email'] = $this->email;
+                Yii::$app->mailer->getView()->params['phone'] = $this->phone;
+                Yii::$app->mailer->getView()->params['address'] = $this->address;
+                Yii::$app->mailer->getView()->params['business'] = $this->business;
+                Yii::$app->mailer->getView()->params['first_payment'] = $this->first_payment;
+                Yii::$app->mailer->getView()->params['contract_amount'] = $this->contract_amount;
+                
+                Yii::$app->mailer->compose([
+                    'html' => 'order-html',
+                    'text' => 'order-text',
+                ],
+                [
+                    'name' => $this->name,
+                    'email' => $this->email,
+                    'phone' => $this->phone,
+                    'address' => $this->address,
+                    'business' => $this->business,
+                    'first_payment' => $this->first_payment,
+                    'contract_amount' => $this->contract_amount,
+                ])
+                ->setFrom([Yii::$app->params['email'] => Yii::$app->name])
+                ->setTo($receiver)
+                ->setSubject(Yii::t('app', 'Заявка от') . ': ' . $this->name)
+                ->send();
+
+                Yii::$app->mailer->getView()->params['name'] = null;
+                Yii::$app->mailer->getView()->params['email'] = null;
+                Yii::$app->mailer->getView()->params['phone'] = null;
+                Yii::$app->mailer->getView()->params['address'] = null;
+                Yii::$app->mailer->getView()->params['business'] = null;
+                Yii::$app->mailer->getView()->params['first_payment'] = null;
+                Yii::$app->mailer->getView()->params['contract_amount'] = null;
+
+            }
+
+        } else {
+            // if updates order
+            
+        }
+    }
 }
