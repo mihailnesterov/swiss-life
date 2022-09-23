695b268c75f772bd63f3aeb75964d192a9b8d2ef
mihailnesterov <mhause@mail.ru>
2022-09-23 18:11:51 +0700

    update: footer

--- widgets/views/footer.php
+++ widgets/views/footer.php
@@ -17,7 +17,7 @@ echo Html::tag(
     . Html::tag(
         'p', 
         Html::encode(\Yii::$app->name) 
-        . ' - '
+        /*. ' - '
         . \Yii::t('app', 'кипрская инвестиционная компания')
         . ' (CIF), '
         . \Yii::t('app', 'контролируемая и регулируемая Кипрской Комиссией по Ценным Бумагам и Биржам')
@@ -25,18 +25,18 @@ echo Html::tag(
         . \Yii::t('app', 'с лицензией') . ' '
         . Html::tag('span', 'XXXXXX', ['class' => 'text-red'])
         . ' ' . \Yii::t('app', 'и регистрационным номером компании') . ' '
-        . Html::tag('span', 'XXXXXXXX', ['class' => 'text-red'])
+        . Html::tag('span', 'XXXXXXXX', ['class' => 'text-red'])*/
     )
-    .Html::a(
+    /*.Html::a(
         \Yii::t('app', 'Лицензия'),
         Url::to(['license', 'lang' => Yii::$app->language]),
         ['class' => 'btn btn-secondary btn-medium']
-    )
+    )*/
 )
-. Html::tag(
+/*. Html::tag(
     'div',
     Html::tag(
         'p', 
         'Адрес и тд'
     )
-);
+)*/;
