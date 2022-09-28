4122862617a4dc9eae267cc47437d474bf247260
mihailnesterov <mhause@mail.ru>
2022-09-28 18:17:32 +0700

    update: config trait add calculate params methods

--- traits/ConfigParamsTrait.php
+++ traits/ConfigParamsTrait.php
@@ -18,4 +18,31 @@ trait ConfigParamsTrait {
         return Yii::$app->params['main-menu'];
     }
 
+    /**
+     * Get calculate investment goals param.
+     * 
+     * @return array
+     */
+    public function getInvestmentGoalsParam() {
+        return Yii::$app->params['calculate']['goals'];
+    }
+
+    /**
+     * Get calculate investment style param.
+     * 
+     * @return array
+     */
+    public function getInvestmentStyleParam() {
+        return Yii::$app->params['calculate']['style'];
+    }
+
+    /**
+     * Get calculate investment results param.
+     * 
+     * @return array
+     */
+    public function getInvestmentResultsParam() {
+        return Yii::$app->params['calculate']['results'];
+    }
+
 }
