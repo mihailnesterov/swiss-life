5403f37ba5952b900fcf10350ede39b3f083da29
mihailnesterov <mhause@mail.ru>
2022-11-06 23:44:27 +0700

    update: investor GoBackBtn component

--- modules/investor/components/InvestorReactApp/src/components/common/buttons/GoBackBtn.js
+++ modules/investor/components/InvestorReactApp/src/components/common/buttons/GoBackBtn.js
@@ -11,10 +11,13 @@ const GoBackBtn = (props) => {
 
     return (
         <Link 
-            className='btn btn-more' 
+            className='btn btn-back' 
             to={`${BASE_URL}/${url}`}
         >
-            <FontAwesomeIcon icon={solid('angles-left')} /> {title ? title : t({
+            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="0.5" y="0.5" width="39" height="39" stroke="#F8F8F8"/>
+                <path d="M9.83073 19.5013L29.1641 19.5013M18.2891 11.043L9.83073 19.5013L18.2891 27.9596" stroke="#F8F8F8" strokeWidth="0.916667" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg> {title ? title : t({
                 id: 'Вернуться в список', 
                 message: 'Вернуться в список'
             })}
