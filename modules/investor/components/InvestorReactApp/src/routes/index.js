feb6a27cf485ec30ca86d76723720d5c89105d4b
mihailnesterov <mhause@mail.ru>
2022-11-06 23:44:28 +0700

    update: investor routes

--- modules/investor/components/InvestorReactApp/src/routes/index.js
+++ modules/investor/components/InvestorReactApp/src/routes/index.js
@@ -7,19 +7,23 @@ import Documents from '../pages/Documents';
 import Investment from '../pages/Investment';
 import InvestmentItemPage from '../pages/InvestmentItemPage';
 import InvestPage from '../pages/InvestPage';
-import Messages from '../pages/Messages';
+import Credit from '../pages/Credit';
+import RepayCredit from '../pages/RepayCredit';
+import MessagesIn from '../pages/MessagesIn';
+import MessagesOut from '../pages/MessagesOut';
+import Seminars from '../pages/Seminars';
 import News from '../pages/News';
 import NewsItemPage from '../pages/NewsItemPage';
 import Profile from '../pages/Profile';
+import ChangePhoto from '../pages/ChangePhoto';
+import ChangePassword from '../pages/ChangePassword';
 import Transactions from '../pages/Transactions';
 import Withdrawals from '../pages/Withdrawals';
 import Transfer from '../pages/Transfer';
+import BankTransfer from '../pages/BankTransfer';
 import AddAccountPage from '../pages/AddAccountPage';
 import Members from '../pages/Members';
 import Contracts from '../pages/Contracts';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export const RouteNames = Enum({
     HOME: `${BASE_URL}`,
@@ -28,13 +32,20 @@ export const RouteNames = Enum({
     INVESTMENT: `${BASE_URL}/investment`,
     INVESTMENT_ITEM_PAGE: `${BASE_URL}/investment/:id`,
     INVEST_PAGE: `${BASE_URL}/invest/:id`,
-    MESSAGES: `${BASE_URL}/messages`,
+    MESSAGES_IN: `${BASE_URL}/messages-in`,
+    MESSAGES_OUT: `${BASE_URL}/messages-out`,
+    CREDIT: `${BASE_URL}/credit`,
+    REPAY_CREDIT: `${BASE_URL}/repay-credit`,
+    SEMINARS: `${BASE_URL}/seminars`,
     NEWS: `${BASE_URL}/news`,
     NEWS_ITEM_PAGE: `${BASE_URL}/news/:id`,
     PROFILE: `${BASE_URL}/profile`,
+    CHANGE_PHOTO: `${BASE_URL}/change-photo`,
+    CHANGE_PASSWORD: `${BASE_URL}/change-password`,
     TRANSACTIONS: `${BASE_URL}/transactions`,
     WITHDRAWALS: `${BASE_URL}/withdrawals`,
     TRANSFER: `${BASE_URL}/transfer`,
+    BANK_TRANSFER: `${BASE_URL}/bank-transfer`,
     ADD_ACCOUNT: `${BASE_URL}/add-account`,
     MEMBERS: `${BASE_URL}/members`,
     CONTRACTS: `${BASE_URL}/contracts`,
@@ -42,20 +53,27 @@ export const RouteNames = Enum({
 });
 
 export const investorRoutes = [
-    {id: 1, path: RouteNames.HOME, element: Home, navbar: true, icon: <FontAwesomeIcon icon={solid('house-user')} />, title: "Кабинет инвестора"},
-    {id: 2, path: RouteNames.TRANSACTIONS, element: Transactions, navbar: true, icon: <FontAwesomeIcon icon={solid('coins')} />, title: "Финансовые операции"},
-    {id: 3, path: RouteNames.WITHDRAWALS, element: Withdrawals, navbar: true, icon: <FontAwesomeIcon icon={solid('arrow-right-arrow-left')} />, title: "Вывод средств"},
-    {id: 4, path: RouteNames.TRANSFER, element: Transfer, navbar: true, icon: <FontAwesomeIcon icon={solid('arrow-right-arrow-left')} />, title: "Перевод средств"},
-    {id: 5, path: RouteNames.INVESTMENT, element: Investment, navbar: true, icon: <FontAwesomeIcon icon={solid('briefcase')} />, title: "Инвестирование"},
-    {id: 6, path: RouteNames.NEWS, element: News, navbar: true, icon: <FontAwesomeIcon icon={solid('newspaper')} />, title: "Новости"},
-    {id: 7, path: RouteNames.MESSAGES, element: Messages, navbar: true, icon: <FontAwesomeIcon icon={solid('envelope')} />, title: "Сообщения"},
-    {id: 8, path: RouteNames.MEMBERS, element: Members, navbar: true, icon: <FontAwesomeIcon icon={solid('user-group')} />, title: "Подписчики"},
-    {id: 9, path: RouteNames.DOCUMENTS, element: Documents, navbar: true, icon: <FontAwesomeIcon icon={solid('folder-open')} />, title: "Документы"},
-    {id: 10, path: RouteNames.CONTRACTS, element: Contracts, navbar: true, icon: <FontAwesomeIcon icon={solid('file-contract')} />, title: "Договоры"},
-    {id: 11, path: RouteNames.PROFILE, element: Profile, navbar: false, icon: <FontAwesomeIcon icon={solid('user-tie')} />, title: "Мой профиль"},
-    {id: 12, path: RouteNames.ERROR404, element: Error404, navbar: false, title: "404 ошибка"},
-    {id: 13, path: RouteNames.NEWS_ITEM_PAGE, element: NewsItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('newspaper')} />, exact: true, title: "Новость"},
-    {id: 14, path: RouteNames.INVESTMENT_ITEM_PAGE, element: InvestmentItemPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Объект инвестирования"},
-    {id: 15, path: RouteNames.INVEST_PAGE, element: InvestPage, navbar: false, icon: <FontAwesomeIcon icon={solid('briefcase')} />, exact: true, title: "Инвестировать в объект"},
-    {id: 16, path: RouteNames.ADD_ACCOUNT, element: AddAccountPage, navbar: false, icon: <FontAwesomeIcon icon={solid('coins')} />, title: "Открыть счет"},
+    {id: 1, path: RouteNames.HOME, element: Home, navbar: true, size: 'normal', title: "Кабинет инвестора"},
+    {id: 2, path: RouteNames.TRANSACTIONS, element: Transactions, navbar: true, size: 'normal', title: "Финансовые операции"},
+    {id: 3, path: RouteNames.TRANSFER, element: Transfer, navbar: true, size: 'small', title: "Внутренний перевод"},
+    {id: 4, path: RouteNames.BANK_TRANSFER, element: BankTransfer, navbar: true, size: 'small', title: "Банковский перевод"},
+    {id: 5, path: RouteNames.INVESTMENT, element: Investment, navbar: true, size: 'small', title: "Инвестирование"},
+    {id: 6, path: RouteNames.WITHDRAWALS, element: Withdrawals, navbar: true, size: 'small', title: "Вывод средств"},
+    {id: 7, path: RouteNames.CREDIT, element: Credit, navbar: true, size: 'normal', title: "Кредит"},
+    {id: 8, path: RouteNames.REPAY_CREDIT, element: RepayCredit, navbar: false, size: 'normal', title: "Погасить кредит"},
+    {id: 9, path: RouteNames.NEWS, element: News, navbar: true, size: 'normal', title: "Новости"},
+    {id: 10, path: RouteNames.MESSAGES_IN, element: MessagesIn, navbar: true, size: 'normal', title: "Сообщения"},
+    {id: 11, path: RouteNames.MESSAGES_OUT, element: MessagesOut, navbar: false, size: 'normal', title: "Сообщения"},
+    {id: 12, path: RouteNames.SEMINARS, element: Seminars, navbar: true, size: 'small', title: "Семинары и тренинги"},
+    {id: 13, path: RouteNames.MEMBERS, element: Members, navbar: true, size: 'normal', title: "Подписчики"},
+    {id: 14, path: RouteNames.DOCUMENTS, element: Documents, navbar: true, size: 'normal', title: "Документы"},
+    {id: 15, path: RouteNames.CONTRACTS, element: Contracts, navbar: true, size: 'small', title: "Договоры"},
+    {id: 16, path: RouteNames.PROFILE, element: Profile, navbar: false, size: 'small', title: "Мой профиль"},
+    {id: 17, path: RouteNames.CHANGE_PHOTO, element: ChangePhoto, navbar: false, size: 'small', title: "Изменить фото"},
+    {id: 18, path: RouteNames.CHANGE_PASSWORD, element: ChangePassword, navbar: false, size: 'small', title: "Изменить пароль"},
+    {id: 19, path: RouteNames.ERROR404, element: Error404, navbar: false, size: 'normal', title: "404 ошибка"},
+    {id: 20, path: RouteNames.NEWS_ITEM_PAGE, element: NewsItemPage, navbar: false, size: 'small', exact: true, title: "Новость"},
+    {id: 21, path: RouteNames.INVESTMENT_ITEM_PAGE, element: InvestmentItemPage, navbar: false, size: 'small', exact: true, title: "Объект инвестирования"},
+    {id: 22, path: RouteNames.INVEST_PAGE, element: InvestPage, navbar: false, size: 'small', exact: true, title: "Инвестировать в объект"},
+    {id: 23, path: RouteNames.ADD_ACCOUNT, element: AddAccountPage, navbar: false, size: 'small', title: "Открыть счет"},
 ];
