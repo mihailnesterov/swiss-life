ad36c8cd9e312c99c8fcfc9f3d883536834c11c8
mihailnesterov <mhause@mail.ru>
2022-11-06 23:44:28 +0700

    update: investor layouts PageLayout

--- modules/investor/components/InvestorReactApp/src/layouts/PageLayout.js
+++ modules/investor/components/InvestorReactApp/src/layouts/PageLayout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/header';
 
 const PageLayout = ({children, title}) => {
     return (
         <div className='page'>
+            <Header />
             <h1>{title}</h1>
             {children}
         </div>
